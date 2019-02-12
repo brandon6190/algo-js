e1ec960bdec45a53da31aeaff9a7b3562fc13e65
brandon6190 <brandon6190@outlook.com>
2019-02-12 11:11:52 -0500

    created test for palindrome

--- test/index.js
+++ test/index.js
@@ -1,8 +1,13 @@
 const { assert } = require('chai');
 const { palindrome } =  require('../src');
 
-describe('Testing.', () => {
-  it('This is only a test', () => {
-    assert.equal(palindrome(), 4);
+describe('palindrome()', () => {
+  it('should return true or false if the input string is a palindrome', () => {
+    assert.equal(palindrome('Anne, I vote more cars race Rome-to-Vienna'), true);
+    assert.equal(palindrome('llama mall'), true);
   });
+
+  it('should return false if input string is not a palindrome', () => {
+    assert.equal(palindrome('jmoney'), false);
+  })
 });
