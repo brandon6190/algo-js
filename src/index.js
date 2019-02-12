d34fb76c815759db690a198410d193e19ce9b268
brandon6190 <brandon6190@outlook.com>
2019-02-12 12:59:19 -0500

    created function isPrime

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ function palindrome(string) {
 }
 
 function isPrime(num) {
-
+  for (let i = 2; i < Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
 }
 
 function nthFibonacci(num, memo=[]) {
