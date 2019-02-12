1a1c06fd319394eba82a32899a9d6a0374142915
brandon6190 <brandon6190@outlook.com>
2019-02-12 11:12:22 -0500

    created function palindrome

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 function palindrome(string) {
-
+  let reverseStr = string.toLowerCase().split('').reverse().join('').replace(/[\W_]/g, '');
+  let regStr = string.toLowerCase().replace(/[\W_]/g, '');
+  return reverseStr === regStr ? true : false;
 }
 
 function isPrime(num) {
